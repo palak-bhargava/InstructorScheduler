3c678d862d3f39e0777224c6c03e30826f2a6f4c
vaishunall <vaishunalla@yahoo.com>
2023-10-07 20:46:25 -0500

    basic project structure set up

---
+++ node-server/models/course_info.js
@@ -0,0 +1,111 @@
+const mongoose = require('mongoose')
+
+const courseSchema = mongoose.Schema(
+    {
+        section_address: {
+            type: String,
+            required: [true]
+        },
+        course_prefix: {
+            type: String,
+            required: [true]
+        },
+        course_number: {
+            type: Number,
+            required: [true]
+        },
+        section: {
+            type: Number,
+            required: [true]
+        },
+        class_number: {
+            type: Number,
+            required: [true]
+        },
+        title: {
+            type: String,
+            required: [true]
+        },
+        topic: {
+            type: String,
+            required: [false]
+        },
+        enrolled_status: {
+            type: String,
+            required: [true]
+        },
+        enrolled_current: {
+            type: Number,
+            required: [false],
+            default: 0
+        },
+        enrolled_max: {
+            type: Number,
+            required: [false],
+            default: 0
+        },
+        instructors: {
+            type: Array,
+            required: [false],
+            default: []
+        },
+        assistants: {
+            type: Array,
+            required: [false],
+            default: []
+        },
+        term: {
+            type: String,
+            required: [true]
+        },
+        session: {
+            type: Number,
+            required: [true]
+        },
+        days: {
+            type: Array,
+            required: [true]
+        },
+        times: {
+            type: String,
+            required: [true]
+        },
+        times_12h: {
+            type: String,
+            required: [true]
+        },
+        location: {
+            type: String,
+            required: [true]
+        },
+        core_area: {
+            type: String,
+            required: [false]
+        },
+        activity_type: {
+            type: String,
+            required: [true]
+        },
+        school: {
+            type: String,
+            required: [true]
+        },
+        dept: {
+            type: String,
+            required: [true]
+        },
+        syllabus: {
+            type: String,
+            required: [false]
+        },
+        textbooks: {
+            type: String,
+            required: [false]
+        }
+
+    }
+)
+
+const Course = mongoose.model('Course', courseSchema)
+
+module.exports = Course
---
+++ node-server/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const Course = require('./models/course_info')
+const app = express();
+const mongoose = require('mongoose')
+
+mongoose
+.connect('REPLACE BEFORE RUNNING')
+.then(() => console.log("Connected to db"))
+.catch((error) => console.error(error))
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("Hello from Palak")
+})
+app.post('/course', async(req, res) => {
+    try{
+        const course = await Course.create(req.body)
+        res.status(200).json(course)
+    }
+    catch (error) { 
+        console.error(error.message)
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+//DEMO:
+//GET
+//POST
+//Next Steps: 
+//1) Get method to get document based on requested parameters, 
+//2) Script to parse JSON file and upload courses to database
+//3) Collection to store instructor preferences,
+//4) Methods to get, post, patch instructor preferences
+
+
+app.listen(3000, () => console.log('Server has started on port 3000'))
