191e7db5f82fff51c41c39221a4bec40c5ff71b0
angelinabiswas <angelinabiswas14@gmail.com>
2023-10-24 03:33:11 -0500

    debugged navbar and banner, finished my preferences, edits made to available courses

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -1,59 +1,25 @@
 <template>
   <v-app>
-    <v-container>
-    <v-app-bar
-        app
-        color="f5f5f5"
-      >
-        <div class="d-flex align-center justify-center" style="width: 100%">
-          <v-img
-
-            class="shrink mr-2"
-            contain
-            :src="require('./assets/IS-logo.png')"
-            transition="scale-transition"
-            width="40"
-          />
-         <v-toolbar-title class="flex text-center">
-          Instructor Scheduler Dashboard
-          </v-toolbar-title>
-        </div>
-        <v-spacer></v-spacer>
-      </v-app-bar>
-      </v-container>
-
-    <br><br>
-      <v-banner
-      lines="one"
-      color="#FFB86F"
-      >
-      <v-banner-text
-      class="ml-15"
-      >
-        New to Instructor Scheduler? Watch this video tutorial to get started
-      </v-banner-text>
-
-        <template v-slot:actions>
-          <v-btn
-          class="mr-15 mb-1"
-          >Tutorial Video</v-btn>
-        </template>
-      </v-banner>
-
     <v-main>
+      <Navbar/>
+      <Banner/>
       <Dashboard/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue';
+import Banner from './components/Banner.vue';
 import Dashboard from './views/dashboard.vue';
 
 export default {
   name: 'App',
 
   components: {
-    Dashboard,
+    Navbar,
+    Banner,
+    Dashboard
   },
 
   data: () => ({
--- vue-client/src/views/dashboard.vue
+++ vue-client/src/views/dashboard.vue
@@ -1,52 +1,50 @@
 <template>
-    <v-container>
+    <v-container class="mt-15">
         <v-row>
             <v-col cols="7">
-            <div class="text-h5 mb-2">My Preferences</div>
-               <v-card
-                    color="#5C9970"
-                    class="rounded-xl"> 
+                <div class="text-h5 mb-2">My Preferences</div>
+                <v-card
+                color="#5C9970"
+                class="rounded-xl"
+                > 
                     <v-container class="spacing-playground pa-5">
-                    <v-row> 
-                        <v-col>
-                            <v-card class="rounded-xl">
-                            <v-container>
-                                <div class="subtitle-1 font-weight-bold">Preferred Courses</div>
-                                    CS 4384 - Automata Theory<br>CS 4875 - Machine Learning
-                            </v-container>
-                            </v-card>
-                        </v-col>
-                        <v-col>
-                            <v-card class="rounded-xl">
-                            <v-container>
-                                <div class="subtitle-1 font-weight-bold">Time Frames</div>
-                                MW  12:00 PM - 4:00 PM<br>TTH  10:00 AM - 2:00 PM
-                            </v-container>
-                            </v-card>
-                        </v-col>
-                    </v-row>
-                    
-                    
-                    <v-row > 
-                     <v-col cols="1" md="6">
-                    <v-btn
-                        color="#FFB86F"
-                        elevation="2"
-                        rounded
-                    >
-                        Edit Preferences
-                    </v-btn> 
-                </v-col>
-                    </v-row>
-                    
-                    </v-container>
-                    
-                </v-card>
-            </v-col>
-            
-          <v-col cols="1">
+                        <v-row> 
+                            <v-col>
+                                <v-card class="rounded-xl">
+                                    <v-container>
+                                        <div class="subtitle-1 font-weight-bold">Preferred Courses</div>
+                                        CS 4384 - Automata Theory<br>CS 4875 - Machine Learning
+                                    </v-container>
+                                </v-card>
+                            </v-col>
 
-        </v-col>
+                            <v-col>
+                                <v-card class="rounded-xl">
+                                    <v-container>
+                                        <div class="subtitle-1 font-weight-bold">Time Frames</div>
+                                        MW  12:00 PM - 4:00 PM<br>TTH  10:00 AM - 2:00 PM
+                                    </v-container>
+                                </v-card>
+                            </v-col>
+                        </v-row>
+                    
+                        <v-row justify-center class="mb-0"> 
+                            <v-btn
+                            color="#FFB86F"
+                            elevation="2"
+                            rounded
+                            class="mx-auto"
+                            >
+                                Edit Preferences
+                            </v-btn> 
+                        </v-row> 
+                    </v-container>
+                </v-card>               
+            </v-col>
+        
+            <v-col cols="1">
+            </v-col>
+
             <v-col cols="4">
                 <div class="text-h5 mb-2">Past Schedules</div>
                 <v-card
@@ -61,88 +59,105 @@
                         block
                         outlined
                         class="mb-2"
-                    >
-                        Fall 2023 
-                    </v-btn>
+                        >
+                            Fall 2023 
+                        </v-btn>
                     
-                    <v-btn
+                        <v-btn
                         color="white"
                         elevation="2"
                         rounded
                         block
                         outlined
                         class="mb-2"
-                    >
-                        Spring 2022
-                    </v-btn> 
-                    <v-btn
+                        >
+                            Spring 2022
+                        </v-btn> 
+                        <v-btn
                         color="white"
                         elevation="2"
                         rounded
                         block
                         outlined
-                    >
-                        Fall 2022
-                    </v-btn> 
+                        >
+                            Fall 2022
+                        </v-btn> 
                     </v-container>
                 </v-card> 
             </v-col>
         </v-row>
-
     
         <div class="text-h5 mb-2 mt-15">Available Courses</div>
+        <v-card
+            color="#5C9970"
+            class="rounded-xl"
+        > 
+            <v-container class="spacing-playground pa-5">
                 <v-card
-                    color="#5C9970"
-                    class="rounded-xl"
-                > 
+                    class = "mb-2 pa-2 rounded-xl font-weight-medium"
+                    color="#FFFFFF"
+                >
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    Class #
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    Course Name 
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    Section
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    Location
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    Days & Time
+                </v-card>
+                <v-btn
+                    color="#FFFFFF"
+                    elevation="2"
+                    rounded
+                    block
+                    outlined
+                    class="mb-2"
+                >
+                    class info
+                </v-btn>
                 
-                    <v-container class="spacing-playground pa-5">
-                    <v-card
-                            class = "mb-2 pa-2 rounded-xl text-decoration-underline"
-                            color="#FFFFFF">
-                            headers
-                        </v-card>
-                    <v-btn
-                        color="#FFFFFF"
-                        elevation="2"
-                        rounded
-                        block
-                        outlined
-                        class="mb-2"
-                    >
-                        class info
-                    </v-btn>
-                    
-                    <v-btn
-                        color="white"
-                        elevation="2"
-                        rounded
-                        block
-                        outlined
-                        class="mb-2"
-                    >
-                        class info
-                    </v-btn> 
-                    <v-btn
-                        color="white"
-                        elevation="2"
-                        rounded
-                        block
-                        outlined
-                    >
-                        class info
-                    </v-btn> 
-                    </v-container>
-                </v-card> 
+                <v-btn
+                    color="white"
+                    elevation="2"
+                    rounded
+                    block
+                    outlined
+                    class="mb-2"
+                >
+                    class info
+                </v-btn> 
+                <v-btn
+                    color="white"
+                    elevation="2"
+                    rounded
+                    block
+                    outlined
+                >
+                    class info
+                </v-btn> 
+            </v-container>
+        </v-card> 
             
         <v-row>
-            ' My Course Schedule
             <v-card
             color="#5C9970"
             class="rounded-xl">
             </v-card>
         </v-row> 
-  </v-container>
+    </v-container>
 </template>
 
 <script>
