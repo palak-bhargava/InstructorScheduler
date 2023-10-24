d9175975f5c2307b133a7889ce4236354b8fe5f9
vedacharthad <vedacharthad@gmail.com>
2023-10-24 01:15:11 -0500

    preferences page

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -1,56 +1,44 @@
 <template>
   <v-app>
     <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+        app
+        color="f5f5f5"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+        <div class="d-flex align-center justify-center" style="width: 100%">
+          <v-img
 
+            class="shrink mr-2"
+            contain
+            :src="require('./assets/IS-logo.png')"
+            transition="scale-transition"
+            width="40"
+          />
+         <v-toolbar-title class="flex text-center">
+          My Preferences
+          </v-toolbar-title>
+          
+          
+        </div>
+
+        <v-spacer></v-spacer>
+      </v-app-bar>
+
+    
+  </v-container>
     <v-main>
-      <HelloWorld/>
+      <Preferences/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Preferences from './views/Preferences.vue';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Preferences,
   },
 
   data: () => ({
---
+++ vue-client/src/views/Preferences.vue
@@ -0,0 +1,36 @@
+<template>
+        <v-card
+            max-width="500"
+            flat
+            class="ma-8 pa-8"
+        >
+        <v-card-title>
+            Course Search
+        </v-card-title>
+            <v-row>
+            <v-col cols="12">
+                <v-autocomplete
+                v-model="values"
+                :items="items"
+                filled
+                rounded
+                outlined
+                background-color = "#5C9970"
+                label="Course Number"
+                color = "white"
+                class = "small-autocomplete"
+                ></v-autocomplete>
+            </v-col>
+            </v-row>
+        </v-card>
+  </template>
+
+  <script>
+    export default {
+    data: () => ({
+      items: ['CS 4348', 'CS 4347', 'CS 4349', 'CS 4375'],
+      values: ['CS 4348'],
+      value: null,
+    }),
+  }
+</script>
