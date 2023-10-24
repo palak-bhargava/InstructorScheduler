c4e52c79b46b1138afd5fe41706a300da521eaef
angelinabiswas <angelinabiswas14@gmail.com>
2023-10-23 20:59:14 -0500

    dashboard page under views (work in progress)

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -1,56 +1,59 @@
 <template>
   <v-app>
+    <v-container>
     <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+        app
+        color="f5f5f5"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+        <div class="d-flex align-center justify-center" style="width: 100%">
+          <v-img
+
+            class="shrink mr-2"
+            contain
+            :src="require('./assets/IS-logo.png')"
+            transition="scale-transition"
+            width="40"
+          />
+         <v-toolbar-title class="flex text-center">
+          Instructor Scheduler Dashboard
+          </v-toolbar-title>
+        </div>
+        <v-spacer></v-spacer>
+      </v-app-bar>
+      </v-container>
+
+    <br><br>
+      <v-banner
+      lines="one"
+      color="#FFB86F"
+      >
+      <v-banner-text
+      class="ml-15"
+      >
+        New to Instructor Scheduler? Watch this video tutorial to get started
+      </v-banner-text>
+
+        <template v-slot:actions>
+          <v-btn
+          class="mr-15 mb-1"
+          >Tutorial Video</v-btn>
+        </template>
+      </v-banner>
 
     <v-main>
-      <HelloWorld/>
+      <Dashboard/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Dashboard from './views/dashboard.vue';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Dashboard,
   },
 
   data: () => ({
---
+++ vue-client/src/views/dashboard.vue
@@ -0,0 +1,80 @@
+<template>
+  
+    <v-container>
+        
+        <v-row class="mt-5">
+            <v-col>
+            <div class="text-h5 mb-5">My Preferences</div>
+               <v-card
+                    color="#5C9970"
+                    class="rounded-xl"> 
+                    
+                    <v-row> 
+                        <v-col>
+                            <v-card>
+                            <v-container>
+                                <div class="subtitle-1 font-weight-bold">Preferred Courses</div>
+                                <v-card>
+                                CS 4384<br>CS4875
+                                </v-card>
+                            </v-container>
+                            </v-card>
+                        </v-col>
+                        <v-col>
+                            <v-card>
+                            <v-container>
+                                <div class="subtitle-1 font-weight-bold">Time Frames</div>
+                            </v-container>
+                            </v-card>
+                        </v-col>
+                    </v-row>
+                </v-card>
+            </v-col>
+            
+            <v-col>
+                <div class="text-h5 mb-2">Past Schedules</div>
+                <v-card
+                color="#5C9970"
+                class="rounded-xl">  
+                    <v-container>
+                        <v-card
+                            class = "mb-2 pa-2 rounded-xl text-decoration-underline"
+                            color="#FFFFFF">
+                            Fall 2023
+                        </v-card>
+                        
+                        <v-card
+                            class = "mb-2 pa-2 rounded-xl text-decoration-underline"
+                            color="#FFFFFF">
+                            Spring 2022
+                        </v-card>
+
+                        <v-card
+                            class = "pa-2 rounded-xl text-decoration-underline"
+                            color="#FFFFFF">
+                            Fall 2022
+                        </v-card>
+                    </v-container>
+                </v-card> 
+            </v-col>
+        </v-row>
+
+        <v-row>
+            Available Courses
+        </v-row>
+            
+        <v-row>
+            My Course Schedule
+            <v-card
+            color="#5C9970"
+            class="rounded-xl">
+            </v-card>
+        </v-row> 
+  </v-container>
+</template>
+
+<script>
+export default {
+  methods: {},
+};
+</script>
