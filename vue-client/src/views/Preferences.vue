57295045dc745d3c9a76ed7582837ab4ea1d5530
angelinabiswas <angelinabiswas14@gmail.com>
2023-11-27 19:02:28 -0600

    took out random event generator, added color scheme

--- vue-client/src/views/Preferences.vue
+++ vue-client/src/views/Preferences.vue
@@ -122,12 +122,8 @@
 <script>
   export default {
     data: () => ({
-      items: ['CS 4348', 'CS 4347', 'CS 4349', 'CS 4375', 'CS 2336', 'CS 2337', 'CS 1336', 'CS 1337', 'CS 3345'],
-      values: ['CS 4348'],
       value: 'null',
       events: [],
-      colors: ['#2196F3', '#3F51B5', '#673AB7', '#00BCD4', '#4CAF50', '#FF9800', '#757575'],
-      names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
       dragEvent: null,
       dragStart: null,
       createEvent: null,
@@ -140,137 +136,93 @@
           this.dragEvent = event
           this.dragTime = null
           this.extendOriginal = null
-    }
-  },
-  startTime (tms) {
-    const mouse = this.toTime(tms)
-
-    if (this.dragEvent && this.dragTime === null) {
-      const start = this.dragEvent.start
-
-      this.dragTime = mouse - start
-    } else {
-      this.createStart = this.roundTime(mouse)
-      this.createEvent = {
-        name: `Event #${this.events.length}`,
-        color: this.rndElement(this.colors),
-        start: this.createStart,
-        end: this.createStart,
-        timed: true
-      }
-
-      this.events.push(this.createEvent)
-    }
-  },
-  extendBottom (event) {
-    this.createEvent = event
-    this.createStart = event.start
-    this.extendOriginal = event.end
-  },
-  mouseMove (tms) {
-    const mouse = this.toTime(tms)
-
-    if (this.dragEvent && this.dragTime !== null) {
-      const start = this.dragEvent.start
-      const end = this.dragEvent.end
-      const duration = end - start
-      const newStartTime = mouse - this.dragTime
-      const newStart = this.roundTime(newStartTime)
-      const newEnd = newStart + duration
-
-      this.dragEvent.start = newStart
-      this.dragEvent.end = newEnd
-    } else if (this.createEvent && this.createStart !== null) {
-      const mouseRounded = this.roundTime(mouse, false)
-      const min = Math.min(mouseRounded, this.createStart)
-      const max = Math.max(mouseRounded, this.createStart)
-
-      this.createEvent.start = min
-      this.createEvent.end = max
-    }
-  },
-  endDrag () {
-    this.dragTime = null
-    this.dragEvent = null
-    this.createEvent = null
-    this.createStart = null
-    this.extendOriginal = null
-  },
-  cancelDrag () {
-    if (this.createEvent) {
-      if (this.extendOriginal) {
-        this.createEvent.end = this.extendOriginal
-      } else {
-        const i = this.events.indexOf(this.createEvent)
-        if (i !== -1) {
-          this.events.splice(i, 1)
         }
-      }
-    }
+      },
+      startTime (tms) {
+        const mouse = this.toTime(tms)
 
-    this.createEvent = null
-    this.createStart = null
-    this.dragTime = null
-    this.dragEvent = null
-  },
-  roundTime (time, down = true) {
-    const roundTo = 15 // minutes
-    const roundDownTime = roundTo * 60 * 1000
+        if (this.dragEvent && this.dragTime === null) {
+          const start = this.dragEvent.start
 
-    return down
-      ? time - time % roundDownTime
-      : time + (roundDownTime - (time % roundDownTime))
-  },
-  toTime (tms) {
-    return new Date(tms.year, tms.month - 1, tms.day, tms.hour, tms.minute).getTime()
-  },
-  getEventColor (event) {
-    const rgb = parseInt(event.color.substring(1), 16)
-    const r = (rgb >> 92) & 0xFF
-    const g = (rgb >> 153) & 0xFF
-    const b = (rgb >> 112) & 0xFF
+          this.dragTime = mouse - start
+        } else {
+          this.createStart = this.roundTime(mouse)
+          this.createEvent = {
+            name: `Event #${this.events.length}`,
+            color: '#FFB86F',
+            start: this.createStart,
+            end: this.createStart,
+            timed: true
+          }
 
-    return event === this.dragEvent
-      ? `rgba(${r}, ${g}, ${b}, 0.7)`
-      : event === this.createEvent
-        ? `rgba(${r}, ${g}, ${b}, 0.7)`
-        : event.color
-  },
-  getEvents ({ start, end }) {
-    const events = []
+        this.events.push(this.createEvent)
+        }
+      },
+      extendBottom (event) {
+        this.createEvent = event
+        this.createStart = event.start
+        this.extendOriginal = event.end
+      },
+      mouseMove (tms) {
+        const mouse = this.toTime(tms)
 
-    const min = new Date(`${start.date}T00:00:00`).getTime()
-    const max = new Date(`${end.date}T23:59:59`).getTime()
-    const days = (max - min) / 86400000
-    const eventCount = this.rnd(days, days + 20)
+        if (this.dragEvent && this.dragTime !== null) {
+          const start = this.dragEvent.start
+          const end = this.dragEvent.end
+          const duration = end - start
+          const newStartTime = mouse - this.dragTime
+          const newStart = this.roundTime(newStartTime)
+          const newEnd = newStart + duration
 
-    for (let i = 0; i < eventCount; i++) {
-      const timed = this.rnd(0, 3) !== 0
-      const firstTimestamp = this.rnd(min, max)
-      const secondTimestamp = this.rnd(2, timed ? 8 : 288) * 900000
-      const start = firstTimestamp - (firstTimestamp % 900000)
-      const end = start + secondTimestamp
+          this.dragEvent.start = newStart
+          this.dragEvent.end = newEnd
+        } else if (this.createEvent && this.createStart !== null) {
+          const mouseRounded = this.roundTime(mouse, false)
+          const min = Math.min(mouseRounded, this.createStart)
+          const max = Math.max(mouseRounded, this.createStart)
 
-      events.push({
-        name: this.rndElement(this.names),
-            color: this.rndElement(this.colors),
-            start,
-            end,
-            timed,
-      })
-    }
+          this.createEvent.start = min
+          this.createEvent.end = max
+        }
+      },
+      endDrag () {
+        this.dragTime = null
+        this.dragEvent = null
+        this.createEvent = null
+        this.createStart = null
+        this.extendOriginal = null
+      },
+      cancelDrag () {
+        if (this.createEvent) {
+          if (this.extendOriginal) {
+            this.createEvent.end = this.extendOriginal
+          } else {
+            const i = this.events.indexOf(this.createEvent)
+            if (i !== -1) {
+              this.events.splice(i, 1)
+            }
+          }
+        }
 
-    this.events = events
-  },
+        this.createEvent = null
+        this.createStart = null
+        this.dragTime = null
+        this.dragEvent = null
+      },
+      roundTime (time, down = true) {
+        const roundTo = 15 // minutes
+        const roundDownTime = roundTo * 60 * 1000
 
-  rnd (a, b) {
-    return Math.floor((b - a + 1) * Math.random()) + a
-  },
-  rndElement (arr) {
-    return arr[this.rnd(0, arr.length - 1)]
-  },
-},
-  }
+        return down
+          ? time - time % roundDownTime
+          : time + (roundDownTime - (time % roundDownTime))
+      },
+      toTime (tms) {
+        return new Date(tms.year, tms.month - 1, tms.day, tms.hour, tms.minute).getTime()
+      },
+
+    },
+}
 </script>
 
 <style scoped lang="scss">
