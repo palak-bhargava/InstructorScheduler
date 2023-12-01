d47657ab57b83d012011f80d943b3510f94f957b
Faiza Rahman <faizarahman301@gmail.com>
2023-11-30 22:05:29 -0600

    changed view

--- vue-client/src/views/AdminView.vue
+++ vue-client/src/views/AdminView.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
      <v-container class="pa-0">
-      <v-app-bar
+    <v-app-bar
         app
         color="f5f5f5"
       >
@@ -19,81 +19,322 @@
         </div>
         <v-spacer></v-spacer>
       </v-app-bar>
-    </v-container>
-    <br>
-
-    <v-row>
-      <v-col cols="3">
-      </v-col>
-      <v-col cols="6">
-        <v-text-field
-            v-model="search"
-            append-icon="mdi-magnify"
-            label="Search"
-            rounded
-            hide-details
-            solo
-            background-color="#5C9970"
-            dark
-        ></v-text-field>
-      </v-col>
-    </v-row>
-    <br>
-
-    <h3>Schedules to Approve</h3>
-    <v-data-table
-      v-model="selected"
-      :headers="headers"
-      :items="available_classes"
-      :single-select="singleSelect"
-      :search="search"
-      :page.sync="page"
-      :items-per-page=9
-      :single-expand="singleExpand"
-      :expanded.sync="expanded"
-      hide-default-footer
-      item-key="number"
-      show-select
-      show-expand
-      class="elevation-2 mt-3"
-      @page-count="pageCount = $event"
+  </v-container>
+    <br><br>
+    <h2>Schedules to Approve</h2><br>
+    <v-expansion-panels
+        dark
+        rounded=30
     >
-    <template v-slot:expanded-item="{ headers, item }">
-      <td :colspan="headers.length">
-        More info about {{ item.name }}
-      </td>
-    </template>
-    </v-data-table>
-    
-    <v-row>
-      <v-col 
-        cols="5"
-        md="5"
-        class="ml-md-auto"
-        align=right
+    <v-expansion-panel
+        rounded
+    >
+      <v-expansion-panel-header
+        color="#5C9970"
+        rounded
       >
-        <v-btn
-          color="#FFB86F"
-          elevation="2"
-          rounded
-          dark
-          class="ml-7 mt-4"
+        <h3>Anjum Chida</h3>
+      </v-expansion-panel-header>
+      <v-expansion-panel-content
+        color="#5C9970"
+        rounded
+      >
+      <br>
+      <v-row>
+        <v-col 
+          cols="9" 
+          class="spacing-playground pa-0"
         >
-          Add To Schedule
-          <v-icon right>
-            mdi-plus
-          </v-icon>
-        </v-btn> 
-      </v-col>
-    </v-row>
+          <v-card
+            class="mx-auto"
+            color="#ffffff"
+            light
+            outlined
+          >
+            <v-list-item three-line>
+              <v-list-item-content>
+                <v-list-item-title class="text-h7">
+                  <b>CS3345:</b> Data Structures, Mon/Wed 1:00 PM - 2:15 PM
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                  <b>CS3345:</b> Data Structures, Mon/Wed 1:00 PM - 2:15 PM
+                </v-list-item-subtitle>
+                <v-list-item-subtitle>
+                  5:30PM - 6:45PM
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            </v-card>
+          </v-col>
 
-    <div class="text-center pt-2">
-        <v-pagination
-          v-model="page"
-          :length="pageCount"
-          color="#5C9970"
-        ></v-pagination>
-    </div>
+          <v-col 
+            cols="3"
+            class="spacing-playground pa-0"
+          >
+            <v-card
+              color="#FFB86F"
+              light
+              outlined
+            >
+              <div class="pt-2 pl-10 ml-12">
+                Approve Class?
+              </div>
+              <v-card-actions>
+                <v-btn
+                  color="#ffffff"
+                  elevation="2"
+                  rounded
+                  light
+                  class="mr-3 mb-1 ml-14"
+                >
+                  Yes 
+                </v-btn> 
+                <v-btn
+                  color="#ffffff"
+                  elevation="2"
+                  rounded
+                  light
+                  class="mb-1 ml-4"
+                >
+                  No
+                </v-btn> 
+              </v-card-actions>
+            </v-card>
+          </v-col>
+         </v-row> 
+        <br><br>
+
+        <v-row>
+        <v-col 
+          cols="9" 
+          class="spacing-playground pa-0"
+        >
+          <v-card
+            class="mx-auto"
+            color="#ffffff"
+            light
+            outlined
+          >
+            <v-list-item three-line>
+              <v-list-item-content>
+                <v-list-item-title class="text-h7">
+                  CS3345
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                  Monday, Wednesday
+                </v-list-item-subtitle>
+                <v-list-item-subtitle>
+                  5:30PM - 6:45PM
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            </v-card>
+          </v-col>
+
+          <v-col 
+            cols="3"
+            class="spacing-playground pa-0"
+          >
+            <v-card
+              color="#FFB86F"
+              light
+              outlined
+            >
+              <div class="pt-2 pl-10 ml-12">
+                Approve Class?
+              </div>
+              <v-card-actions>
+                <v-btn
+                  color="#ffffff"
+                  elevation="2"
+                  rounded
+                  light
+                  class="mr-3 mb-1 ml-14"
+                >
+                  Yes 
+                </v-btn> 
+                <v-btn
+                  color="#ffffff"
+                  elevation="2"
+                  rounded
+                  light
+                  class="mb-1 ml-4"
+                >
+                  No
+                </v-btn> 
+              </v-card-actions>
+            </v-card>
+          </v-col>
+         </v-row> 
+        <br><br>
+
+        <v-row>
+        <v-col 
+          cols="9" 
+          class="spacing-playground pa-0"
+        >
+          <v-card
+            class="mx-auto"
+            color="#ffffff"
+            light
+            outlined
+          >
+            <v-list-item three-line>
+              <v-list-item-content>
+                <v-list-item-title class="text-h7">
+                  CS3345
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                  Monday, Wednesday
+                </v-list-item-subtitle>
+                <v-list-item-subtitle>
+                  5:30PM - 6:45PM
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            </v-card>
+          </v-col>
+
+          <v-col 
+            cols="3"
+            class="spacing-playground pa-0"
+          >
+            <v-card
+              color="#FFB86F"
+              light
+              outlined
+            >
+              <div class="pt-2 pl-10 ml-12">
+                Approve Class?
+              </div>
+              <v-card-actions>
+                <v-btn
+                  color="#ffffff"
+                  elevation="2"
+                  rounded
+                  light
+                  class="mr-3 mb-1 ml-14"
+                >
+                  Yes 
+                </v-btn> 
+                <v-btn
+                  color="#ffffff"
+                  elevation="2"
+                  rounded
+                  light
+                  class="mb-1 ml-4"
+                >
+                  No
+                </v-btn> 
+              </v-card-actions>
+            </v-card>
+          </v-col>
+         </v-row> 
+         <br><br>
+
+        <v-row>
+        <v-col 
+          cols="9" 
+          class="spacing-playground pa-0"
+        >
+          <v-card
+            class="mx-auto"
+            color="#ffffff"
+            light
+            outlined
+          >
+            <v-list-item three-line>
+              <v-list-item-content>
+                <v-list-item-title class="text-h7">
+                  CS3345
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                  Monday, Wednesday
+                </v-list-item-subtitle>
+                <v-list-item-subtitle>
+                  5:30PM - 6:45PM
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            </v-card>
+          </v-col>
+
+          <v-col 
+            cols="3"
+            class="spacing-playground pa-0"
+          >
+            <v-card
+              color="#FFB86F"
+              light
+              outlined
+            >
+              <div class="pt-2 pl-10 ml-12">
+                Approve Class?
+              </div>
+              <v-card-actions>
+                <v-btn
+                  color="#ffffff"
+                  elevation="2"
+                  rounded
+                  light
+                  class="mr-3 mb-1 ml-14"
+                >
+                  Yes 
+                </v-btn> 
+                <v-btn
+                  color="#ffffff"
+                  elevation="2"
+                  rounded
+                  light
+                  class="mb-1 ml-4"
+                >
+                  No
+                </v-btn> 
+              </v-card-actions>
+            </v-card>
+          </v-col>
+         </v-row> 
+         <br>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+
+  <br>
+  <v-expansion-panels
+    dark
+  >
+    <v-expansion-panel>
+      <v-expansion-panel-header
+        color="#5C9970"
+      >
+        <h3>John Cole</h3>
+      </v-expansion-panel-header>
+      <v-expansion-panel-content
+        color="#5C9970"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+
+  <br>
+  <v-expansion-panels
+    dark
+  >
+    <v-expansion-panel>
+      <v-expansion-panel-header
+        color="#5C9970"
+      >
+        <h3>Karen Mazidi</h3>
+      </v-expansion-panel-header>
+      <v-expansion-panel-content
+        color="#5C9970"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+  
   </v-container>
 </template>
 
@@ -102,167 +343,11 @@
   export default {
     name: 'AdminView',
 
-    data () {
-      return {
-        page: 1,
-        pageCount: 0,
-        itemsPerPage: 9,
-        search: '',
-        singleSelect: false,
-        selected: [],
-        headers: [
-          {
-            text: 'Class #',
-            align: 'start',
-            sortable: false,
-            value: 'number',
-          },
-          { text: 'Course Name', value: 'name' },
-          { text: 'Section', value: 'section' },
-          { text: 'Location', value: 'location' },
-          { text: 'Days', value: 'days' },
-          { text: 'Time', value: 'time' },
-        ],
-        available_classes: [
-          {
-            number: 123456,
-            name: 'Data Structures',
-            section: '003',
-            location: 'ECSW 2.106',
-            days: 'TTH',
-            time: '10:00AM - 11:15AM',
-          },
-          {
-            number: 574930,
-            name: 'Machine Learning',
-            section: '003',
-            location: 'ECSW 2.106',
-            days: 'TTH',
-            time: '10:00AM - 11:15AM',
-          },
-          {
-            number: 890273,
-            name: 'Advanced Algorithms',
-            section: '003',
-            location: 'ECSW 2.106',
-            days: 'TTH',
-            time: '10:00AM - 11:15AM',
-          },
-          {
-            number: 469320,
-            name: 'Data Structures',
-            section: '004',
-            location: 'ECSW 2.106',
-            days: 'TTH',
-            time: '10:00AM - 11:15AM',
-          },
-          {
-            number: 738209,
-            name: 'Data Structures',
-            section: '003',
-            location: 'ECSW 2.106',
-            days: 'TTH',
-            time: '10:00AM - 11:15AM',
-          },
-          {
-            number: 103864,
-            name: 'Machine Learning',
-            section: '003',
-            location: 'ECSW 2.106',
-            days: 'TTH',
-            time: '10:00AM - 11:15AM',
-          },
-          {
-            number: 310432,
-            name: 'Advanced Algorithms',
-            section: '003',
-            location: 'ECSW 2.106',
-            days: 'TTH',
-            time: '10:00AM - 11:15AM',
-          },
-          {
-            number: 595743,
-            name: 'Data Structures',
-            section: '004',
-            location: 'ECSW 2.106',
-            days: 'TTH',
-            time: '10:00AM - 11:15AM',
-          },
-          {
-            number: 984633,
-            name: 'Data Structures',
-            section: '003',
-            location: 'ECSW 2.106',
-            days: 'TTH',
-            time: '10:00AM - 11:15AM',
-          },
-          {
-            number: 464839,
-            name: 'Machine Learning',
-            section: '003',
-            location: 'ECSW 2.106',
-            days: 'TTH',
-            time: '10:00AM - 11:15AM',
-          },
-          {
-            number: 203474,
-            name: 'Advanced Algorithms',
-            section: '003',
-            location: 'ECSW 2.106',
-            days: 'TTH',
-            time: '10:00AM - 11:15AM',
-          },
-          {
-            number: 230432,
-            name: 'Data Structures',
-            section: '004',
-            location: 'ECSW 2.106',
-            days: 'TTH',
-            time: '10:00AM - 11:15AM',
-          },
-          {
-            number: 658494,
-            name: 'Data Structures',
-            section: '003',
-            location: 'ECSW 2.106',
-            days: 'TTH',
-            time: '10:00AM - 11:15AM',
-          },
-          {
-            number: 134556,
-            name: 'Machine Learning',
-            section: '003',
-            location: 'ECSW 2.106',
-            days: 'TTH',
-            time: '10:00AM - 11:15AM',
-          },
-          {
-            number: 122334,
-            name: 'Advanced Algorithms',
-            section: '003',
-            location: 'ECSW 2.106',
-            days: 'TTH',
-            time: '10:00AM - 11:15AM',
-          },
-          {
-            number: 111111,
-            name: 'Data Structures',
-            section: '004',
-            location: 'ECSW 2.106',
-            days: 'TTH',
-            time: '10:00AM - 11:15AM',
-          },
-        ],
-      }
-    },
+    data: () => ({
+      
+    }),
   }
 </script>
 
 <style>
-.v-data-table-header {
-  background-color: #ffb86f !important;
-}
-.v-data-table__expanded.v-data-table__expanded__content {
-  box-shadow: none !important;
-}
 </style>
