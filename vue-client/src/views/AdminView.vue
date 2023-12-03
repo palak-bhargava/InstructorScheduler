abb6547e6e40bbd38f0d65e034586f3ea882141a
Faiza Rahman <faizarahman301@gmail.com>
2023-12-03 15:45:48 -0600

    in progress

--- vue-client/src/views/AdminView.vue
+++ vue-client/src/views/AdminView.vue
@@ -26,182 +26,107 @@
         dark
         rounded=30
     >
-    <v-expansion-panel
-        rounded
-    >
-      <v-expansion-panel-header
-        color="#5C9970"
-        rounded
-      >
-        <h3>Anjum Chida</h3>
-        <v-btn
-          color="#FFB86F"
-          elevation="2"
-          rounded
-          light
-          style="max-width: 230px;"
-        >
-          Generate Schedule
-        </v-btn> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      </v-expansion-panel-header>
-      <v-expansion-panel-content
-        color="#5C9970"
-        rounded
-      >
+      <v-expansion-panel v-for="(schedule, index) in allSchedules" :key="index" rounded>
+        <v-expansion-panel-header color="#5C9970" rounded>
+          <h3>{{ schedule.instructor_name }}</h3>
+          <v-btn
+            color="#FFB86F"
+            elevation="2"
+            rounded
+            light
+            style="max-width: 230px;"
+            @click="showInstructorSchedule(schedule)"
+          >
+            Generate Schedule
+          </v-btn> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </v-expansion-panel-header>
+      <v-expansion-panel-content color="#5C9970" rounded>
       <br>
       <v-row>
-        <v-col 
-          cols="9" 
-          class="spacing-playground pa-0"
-        >
-          <v-card
-            class="mx-auto"
-            color="#ffffff"
-            light
-            outlined
-          >
-            <v-list-item three-line>
-              <v-list-item-content>
-                <v-list-item-title class="text-h7">
-                  <b>CS3345 Data Structures</b> 
-                </v-list-item-title>
-                <v-list-item-subtitle>
-                Monday, Wednesday | 1:00 PM - 2:15 PM
-                </v-list-item-subtitle>
-                <br>
-                <v-list-item-title class="text-h7">
-                  <b>CS2336 Computer Science II</b> 
-                </v-list-item-title>
-                <v-list-item-subtitle>
-                Monday, Wednesday | 2:00 PM - 3:15 PM
-                </v-list-item-subtitle>
-                <br>
-                <v-list-item-title class="text-h7">
-                  <b>CS4485 Automata Theory</b> 
-                </v-list-item-title>
-                <v-list-item-subtitle>
-                Tuesday, Thursday | 1:00 PM - 2:15 PM
-                </v-list-item-subtitle>
-                <br>
-                <v-list-item-title class="text-h7">
-                  <b>CS3349 Advanced Algorithms</b> 
-                </v-list-item-title>
-                <v-list-item-subtitle>
-                Tuesday, Thursday | 8:00 PM - 9:15 PM
-                </v-list-item-subtitle>
-                <br>
-              </v-list-item-content>
-            </v-list-item>
-            </v-card>
-          </v-col>
+    <v-col cols="9" class="spacing-playground pa-0">
+      <v-card class="mx-auto" color="#ffffff" light outlined>
+        <v-list-item three-line v-for="(course, index) in schedule.courses" :key="index">
+          <v-list-item-content>
+          
+            <v-list-item-title class="text-h7">
+            <div v-if="course">
+              {{ course.course_number }}
+            </div>
+              <b></b> 
+            </v-list-item-title>
+            <!-- Add other details as needed -->
+          </v-list-item-content>
+        </v-list-item>
+   
+      </v-card>
+    </v-col>
 
-          <v-col 
-            cols="3"
-            class="spacing-playground pa-0"
-          >
-            <v-card
-              color="#5C9970"
-              light
-              outlined
-            >
-              <div class="pl-8 ml-12">
-                <b>Approve Schedule?</b>
-              </div>
-              <v-card-actions>
-                <v-btn
-                  color="#FFB86F"
-                  elevation="2"
-                  rounded
-                  light
-                  class="mr-3 mb-1 ml-15"
-                >
-                  Yes 
-                </v-btn> 
-                <v-btn
-                  color="#FFB86F"
-                  elevation="2"
-                  rounded
-                  light
-                  class="mb-1 ml-4"
-                >
-                  No
-                </v-btn> 
-              </v-card-actions>
-            </v-card>
-          </v-col>
-         </v-row> 
-        <br>
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-  </v-expansion-panels>
-
-  <br>
-  <v-expansion-panels
-    dark
-  >
-    <v-expansion-panel>
-      <v-expansion-panel-header
-        color="#5C9970"
-        rounded
-      >
-        <h3>John Cole</h3>
-        <v-btn
-          color="#FFB86F"
-          elevation="2"
-          rounded
-          light
-          style="max-width: 230px;"
-        >
-          Generate Schedule
-        </v-btn> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      </v-expansion-panel-header>
-      <v-expansion-panel-content
-        color="#5C9970"
-      >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-  </v-expansion-panels>
-
-  <br>
-  <v-expansion-panels
-    dark
-  >
-    <v-expansion-panel>
-      <v-expansion-panel-header
-        color="#5C9970"
-        rounded
-      >
-        <h3>Karen Mazidi</h3>
-        <v-btn
-          color="#FFB86F"
-          elevation="2"
-          rounded
-          light
-          style="max-width: 230px;"
-        >
-          Generate Schedule
-        </v-btn> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      </v-expansion-panel-header>
-      <v-expansion-panel-content
-        color="#5C9970"
-      >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-  </v-expansion-panels>
   
+        <v-col cols="3" class="spacing-playground pa-0">
+          <v-card color="#5C9970" light outlined>
+            <div class="pl-8 ml-12">
+              <b>Approve Schedule?</b>
+            </div>
+            <v-card-actions>
+              <v-btn color="#FFB86F" elevation="2" rounded light class="mr-3 mb-1 ml-15">
+                Yes
+              </v-btn>
+              <v-btn color="#FFB86F" elevation="2" rounded light class="mb-1 ml-4">
+                No
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
   </v-container>
 </template>
 
 <script>
+  import axios from "axios"
 
   export default {
     name: 'AdminView',
 
-    data: () => ({
+    data() {
+      return {
+        allSchedules: [],
+        instructorSchedule: [],
+      };
+    },
+    created() {
+      this.getAllSchedules();
+    },
+    methods: {
       
-    }),
+      async getAllSchedules() {
+        try {
+          const response = await axios.get(`http://localhost:3000/instructorschedules`);
+          this.allSchedules = response.data; // Assign the data to the pastClasses property
+        } 
+        catch (error) {
+          console.error(error);
+        }
+        console.log("all schedules array:", this.allSchedules);
+      
+      },
+
+      showInstructorSchedule(schedule) {
+        // Assuming schedule contains class_schedules field with an array of schedules
+        this.instructorSchedule = schedule;
+        // Optionally, you can log the schedule for debugging or verification
+        console.log("Instructor Schedule:", this.instructorSchedule);
+      },
+      showClass(course) {
+        // Assuming schedule contains class_schedules field with an array of schedules
+        this.instructorSchedule = course;
+        // Optionally, you can log the schedule for debugging or verification
+        console.log("Classes:", this.instructorSchedule);
+       
+      },
+    },
   }
 </script>
 
