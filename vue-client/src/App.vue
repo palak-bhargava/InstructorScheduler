9c68f667ca0b59888046c68ef22f164e0ba927df
Palak Bhargava <palak_bhargava@hotmail.com>
2023-12-04 03:31:40 -0600

    added routing to preferences

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -1,23 +1,14 @@
 <template>
   <v-app>
     <v-main>
-      <AdminView/>
+      <router-view>
+      </router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import AdminView from './views/AdminView';
-
 export default {
   name: 'App',
-
-  components: {
-    AdminView,
-  },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
---
+++ vue-client/src/router.js
@@ -0,0 +1,58 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+// Import your components
+import LoginPage from '@/views/LoginPage.vue';
+import AdminLogin from '@/views/AdminLogin.vue';
+import Dashboard from '@/views/dashboard.vue';
+import AvailableCourses from '@/views/AvailableCourses.vue';
+import PastSchedules from '@/views/PastSchedules.vue';
+import AdminView from '@/views/AdminView.vue';
+import Preferences from '@/views/preferences.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+
+    {
+    path: '/',
+    component: LoginPage,
+    name: 'Login',
+  },
+  {
+    path: '/admin-login',
+    component: AdminLogin,
+    name: 'AdminLogin',
+  },
+  {
+    path:'/dashboard',
+    component: Dashboard,
+    name: 'Dashboard',
+  },
+  {
+    path: '/available-courses',
+    component: AvailableCourses,
+    name: 'AvailableCourses',
+  },
+  {
+    path: '/past-schedules',
+    component: PastSchedules,
+    name: 'PastSchedules',
+  },
+  {
+    path: '/admin-view',
+    component: AdminView,
+    name: 'AdminView',
+  },
+  {
+    path: '/my-preferences',
+    component: Preferences,
+    name: 'Preferences',
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
